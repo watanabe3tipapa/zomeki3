f680f95327d3f4d3d05241ae1b14882a7ccf5ae2
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-04-12 16:23:11 +0900

    [zomekiv3-spec:00124] Remove obsoleted file transfer function

--- app/models/sys/publisher.rb
+++ app/models/sys/publisher.rb
@@ -4,7 +4,7 @@ class Sys::Publisher < ApplicationRecord
   belongs_to :publishable, polymorphic: true
 
   before_validation :modify_path
-  before_save :check_path
+  before_save :remove_files_in_old_path
   before_destroy :remove_files
 
   def site_id
@@ -15,6 +15,8 @@ class Sys::Publisher < ApplicationRecord
     @site ||= Cms::Site.find_by(id: site_id)
   end
 
+  private
+
   def modify_path
     self.path = path.gsub(/^#{Rails.root.to_s}/, '.')
   end
@@ -22,28 +24,15 @@ class Sys::Publisher < ApplicationRecord
   def remove_files(options = {})
     up_path = options[:path] || path
     up_path = ::File.expand_path(path, Rails.root) if up_path.to_s.slice(0, 1) == '/'
+
     FileUtils.rm(up_path) if FileTest.exist?(up_path)
     FileUtils.rm("#{up_path}.mp3") if FileTest.exist?("#{up_path}.mp3")
     FileUtils.rmdir(::File.dirname(path)) rescue nil
-    transcribe self, options  if Zomeki.config.application['sys.log_closed_page']
     return true
   end
 
-protected
-  def check_path
-    remove_files(:path => path_was) if !path_was.blank? && path_changed?
+  def remove_files_in_old_path
+    remove_files(path: path_was) if path_changed? && path_was.present?
     return true
-  end
-
-  def transcribe(publisher, options={})
-    cl = Sys::Closer.new(publisher.attributes)
-    cl.published_at   = publisher.created_at.clone rescue nil;
-    cl.republished_at = publisher.updated_at.clone rescue nil;
-    cl.created_at     = nil
-    cl.updated_at     = nil
-    cl.path = options[:path] if options[:path]
-    cl.save!
-  rescue
-    false
   end
 end
